74c7afba691ee607125790eaac797ea60c19ca0c
toprogramm <toprogramm@gmail.com>
2023-05-07 01:25:48 +0600

    cosmetics and comments moved to  how_to_check.md

--- 24words.py
+++ 24words.py
@@ -1,9 +1,3 @@
-# create by description https://forum.e4pool.com/viewtopic.php?t=946
-# check correct seed phrase on https://iancoleman.io/bip39/
-# entropy and sha256 for check converting 
-#entropy = '1001000101010011101011111000101011101111011110100001110001001010100100001001011010011100110001001001110100000010000110110001010010111111011001011011011111100001111100000101111110001110010000100010011100001100111000111011100101100110110000011101100101101111'
-#hex = c716ada6e8ebd952598bb5622ccbd119620a9ad0ccbcf29ad55077a38603d61b - hex result from ↑ entropy /(binary mode)
-#chunks = mule output tired ten peace census drastic squeeze setup trend man city wagon swim march armor mixture bachelor segment shuffle nose genius grant wedding 
 import binascii
 import hashlib
 import random
@@ -26,7 +20,7 @@ def get_hex(binary_number):
 def binary_seed_phrase():
     tail = get_hex(entropy)[slice(2)]
     binary_tail = base16_to_base2(tail, 8)
-    binary_full_phrase = entropy+binary_tail
+    binary_full_phrase = entropy + binary_tail
     return binary_full_phrase
 
 def cut_by(value, number_of_elements):
